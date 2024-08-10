e9faf1518012bd226bad7da4f5f907ffcbbffe8d
Jafnak <jafnakusman@gmail.com>
2024-08-10 18:52:22 +0530

    adminSignUp updated

---
+++ app.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+const cors = require("cors")
+const express = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const loginModel = require("./models/admin")
+const app = express()
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://Jafna02:jafna9074@cluster0.icijy.mongodb.net/RescueDb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.get("/tasks",(req,res)=>{
+    res.json({"status":"success"})
+})
+
+app.post("/adminSignup",(req,res)=>{
+    let input=req.body
+    let hashedpassword=bcrypt.hashSync(input.password,10)
+    console.log(hashedpassword)
+    input.password=hashedpassword
+    console.log(input)
+    let result=new loginModel(input)
+    result.save()
+    res.json({"status":"success"})
+})
+
+app.listen(8080,()=>{
+    console.log("server stated")
+})
